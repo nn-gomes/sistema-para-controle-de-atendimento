eca0dea06a9898acfdc5fce057e5e1ee7ecb1277
lucasjstn <lucas7justino@gmail.com>
2024-04-14 21:41:02 -0300

    ad:lista funcionando

--- src/app/services/password.service.ts
+++ src/app/services/password.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class PasswordService {
   itens: string[] = ['Item 1', 'Item 2', 'Item 3'];
-  
+
   constructor() { }
 
   senhas = [
@@ -16,10 +16,16 @@ export class PasswordService {
       tm_geracao: '2024-04-01 19:10:09',
     }, 
     {
-      icon: 'accessibility',
-      color: 'primary',
-      senha: '240401-SP01',
+      icon: 'document',
+      color: 'warning',
+      senha: '240401-SE01',
       tm_geracao: '2024-04-01 19:10:09',
-    }, 
+    },
+    {
+      icon: 'home',
+      color: 'dark',
+      senha: '240401-SG01',
+      tm_geracao: '2024-04-01 19:10:09',
+    },
   ]
 }
